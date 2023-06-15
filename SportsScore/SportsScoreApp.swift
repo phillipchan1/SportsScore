38d378d2c73c672cff05364939d17e4238e826b1
phillipchan1 <phillipchan1@gmail.com>
2023-06-15 14:12:01 -0700

    Initial Commit

---
+++ SportsScore/SportsScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsScoreApp.swift
+//  SportsScore
+//
+//  Created by Phil Chan on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SportsScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
